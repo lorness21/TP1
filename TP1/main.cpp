696c3ed218b12ebcd76a61f33a1d3aa167ccdf40
lorness <subleta@sfr.fr>
2023-04-24 15:21:25 +0200

    rectangle

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
 int main() {
-	std::cout << "hello words !" << std::endl;
+	int largeur, longueur;
+	std::cin >> largeur >> longueur;
+
+	for (int i_l = 0; i_l < largeur; i_l++) {
+		for (int i_L = 0; i_L < longueur;i_L++) {
+			if (i_l == 0 || i_l == largeur - 1)
+				std::cout << "-";
+			else if (i_L == 0 || i_L == longueur - 1)
+				std::cout << "|";
+			else
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+
 	return 0;
 }
