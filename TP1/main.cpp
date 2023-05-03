6664cec5857cc7bfa0e8c07d740445b3e85277ad
Cola <cola@adresseMail.me>
2023-05-03 22:30:49 +0200

    affichage console + boucle

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -3,51 +3,73 @@
 
 int main()
 {
-	int choix;
-	std::cout << "rectangle(1) ou cercle(2) : ";
-	std::cin >> choix;
-	if (choix == 2) {
+	int choix = 1;
 
-		int diametre;
-		std::cout << "Rentrer valeur" << std::endl;
-		std::cin >> diametre;
-		double thickness = 0.5;
-		double radius = (diametre - 1.0) / 2.0;
-		double rIn = radius - thickness;
-		double rOut = radius + thickness;
+	while (choix != 0)
+	{
+		std::cout << "rectangle : 1 " << std::endl << "cercle    : 2 " << std::endl << "sortir    : 0 " << std::endl;
+		std::cin >> choix;
 
-		for (int y = 0; y < diametre; y++)
+		if (choix == 2)
 		{
-			for (int x = 0; x < diametre; x++)
+			int diametre;
+
+			std::cout << "Rentrer valeur" << std::endl;
+			std::cin >> diametre;
+
+			double thickness = 0.5;
+			double radius = (diametre - 1.0) / 2.0;
+			double rIn = radius - thickness;
+			double rOut = radius + thickness;
+
+			for (int y = 0; y < diametre; y++)
 			{
-				double value = (x - radius) * (x - radius) + (y - radius) * (y - radius);
-				if (value >= rIn * rIn && value <= rOut * rOut)
+				for (int x = 0; x < diametre; x++)
 				{
-					std::cout << '#';
+					double value = (x - radius) * (x - radius) + (y - radius) * (y - radius);
+					if (value >= rIn * rIn && value <= rOut * rOut)
+					{
+						std::cout << '#';
+					}
+					else
+					{
+						std::cout << ' ';
+					}
 				}
-				else
-				{
-					std::cout << ' ';
-				}
+				std::cout << std::endl;
 			}
-			std::cout << std::endl;
 		}
-	}
 
-	else {
-		int largeur, longueur;
-		std::cin >> largeur >> longueur;
+		else if (choix == 1)
+		{
+			int largeur, longueur;
 
-		for (int i_l = 0; i_l < largeur; i_l++) {
-			for (int i_L = 0; i_L < longueur;i_L++) {
-				if (i_l == 0 || i_l == largeur - 1)
-					std::cout << "#";
-				else if (i_L == 0 || i_L == longueur - 1)
-					std::cout << "#";
-				else
-					std::cout << " ";
+			std::cout << "Rentrer une largeur : " << std::endl;
+			std::cin >> largeur;
+			std::cout << "Rentrer une longueur : " << std::endl;
+			std::cin >> longueur;
+
+			for (int i_l = 0; i_l < largeur; i_l++)
+			{
+				for (int i_L = 0; i_L < longueur; i_L++)
+				{
+					if (i_l == 0 || i_l == largeur - 1)
+					{
+						std::cout << "#";
+					}
+
+					else if (i_L == 0 || i_L == longueur - 1)
+					{
+						std::cout << "#";
+					}
+
+					else
+					{
+						std::cout << " ";
+					}
+				}
+				std::cout << std::endl;
 			}
-			std::cout << std::endl;
 		}
 	}
 	return 0;
