98b30ee6751dcf6c12bbc4df766e04e645e0a1c0
JinXera <131671034+JinXera@users.noreply.github.com>
2023-05-03 16:02:43 +0200

    Circle

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+#include <math.h>
 
-int main() {
-	int largeur, longueur;
-	std::cin >> largeur >> longueur;
+int main()
+{
+	int diametre;
+	std::cout << "Rentrer valeur" << std::endl;
+	std::cin >> diametre;
+	double thickness = 0.5;
+	double radius = (diametre - 1.0) / 2.0;
+	double rIn = radius - thickness;
+	double rOut = radius + thickness;
+	
 
-	for (int i_l = 0; i_l < largeur; i_l++) {
-		for (int i_L = 0; i_L < longueur;i_L++) {
-			if (i_l == 0 || i_l == largeur - 1)
-				std::cout << "#";
-			else if (i_L == 0 || i_L == longueur - 1)
-				std::cout << "#";
+	for (int y = 0; y < diametre ; y++)
+	{
+		for (int x = 0; x < diametre; x++)
+		{
+			double value = (x - radius) * (x - radius) + (y - radius) * (y - radius);
+			if (value >= rIn * rIn && value <= rOut * rOut)
+			{
+				std::cout << '#';
+			}
 			else
-				std::cout << " ";
+			{
+				std::cout << ' ';
+			}
 		}
 		std::cout << std::endl;
 	}
-
-	return 0;
+	
 }
