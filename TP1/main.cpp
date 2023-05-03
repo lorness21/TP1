02eafd3c37593b37b4b8640ca75840725a2a3abb
lorness <subleta@sfr.fr>
2023-05-03 15:50:36 +0200

    rectangle2

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -7,9 +7,9 @@ int main() {
 	for (int i_l = 0; i_l < largeur; i_l++) {
 		for (int i_L = 0; i_L < longueur;i_L++) {
 			if (i_l == 0 || i_l == largeur - 1)
-				std::cout << "-";
+				std::cout << "#";
 			else if (i_L == 0 || i_L == longueur - 1)
-				std::cout << "|";
+				std::cout << "#";
 			else
 				std::cout << " ";
 		}
