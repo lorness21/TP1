52c9efb393bdd6e10557aa161b4ad6e77b56a299
Cola <cola@adresseMail.me>
2023-05-03 23:39:16 +0200

    ajout de OpenCV

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <math.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
 
 int main()
 {
 	int choix = 1;
+
+	Mat BlackMatrix(1000, 1000, CV_8UC3, Scalar(0, 0, 0)); //Declring the Black matrix
+	Point center(500, 500); //Starting Point of the line
+	double thickness = 2; //Thickness of the line
+	namedWindow("OpenCV Image"); //Declaring a window to show the line
 
 	while (choix != 0)
 	{
@@ -12,19 +22,23 @@ int main()
 
 		if (choix == 2)
 		{
-			int diametre;
+			int diameter;
 
-			std::cout << "Rentrer valeur" << std::endl;
-			std::cin >> diametre;
+			std::cout << "Rentrer un diametre :" << std::endl;
+			std::cin >> diameter;
 
-			double thickness = 0.5;
-			double radius = (diametre - 1.0) / 2.0;
-			double rIn = radius - thickness;
-			double rOut = radius + thickness;
+			Scalar line_Color(0, 0, 255);
+			double radius = diameter / 2.0;
+			circle(BlackMatrix, center, radius, line_Color, thickness); // using line() function to draw the line
+			imshow("OpenCV Image", BlackMatrix); //Showing the line of the circle
+			waitKey(0); //Waiting for Keystroke
 
-			for (int y = 0; y < diametre; y++)
+			double rIn = ((diameter - 1) / 2) - thickness;
+			double rOut = ((diameter - 1) / 2) + thickness;
+
+			for (int y = 0; y < diameter; y++)
 			{
-				for (int x = 0; x < diametre; x++)
+				for (int x = 0; x < diameter; x++)
 				{
 					double value = (x - radius) * (x - radius) + (y - radius) * (y - radius);
 					if (value >= rIn * rIn && value <= rOut * rOut)
@@ -48,6 +62,13 @@ int main()
 			std::cin >> largeur;
 			std::cout << "Rentrer une longueur : " << std::endl;
 			std::cin >> longueur;
+
+			Scalar line_Color(0, 255, 0); //Color of the rectangle
+			Point Starting(500, 500); //Declaring the starting coordinate
+			Point Ending(500 + longueur,500 + largeur); //Declaring the ending coordinate
+			rectangle(BlackMatrix, Starting, Ending, line_Color, thickness); //Drawing the rectangle
+			imshow("OpenCV Image", BlackMatrix); //Showing the rectangle
+			waitKey(0); //Waiting for Keystroke
 
 			for (int i_l = 0; i_l < largeur; i_l++)
 			{
